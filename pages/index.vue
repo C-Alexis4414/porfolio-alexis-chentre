9320cd3bc8a59aec5b9fe111a956912e76dad377
alexisc <a.chentreu@protonmail.com>
2023-09-17 18:06:43 +0200

    Presentation

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { HandRaisedIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+    <section>
+        <div class="flex flex-row">
+            <HandRaisedIcon class="h-6 w-6 text-blue-500"/>
+            <h1 class="text-emerald-400 font-semibold m-2">Bonjour, je m'appelle Alexis Chentre !</h1>
+        </div>
+        <p class="text-base text-lime-200 m-4 italic">Développeur web</p>
+    </section>
+</template>
