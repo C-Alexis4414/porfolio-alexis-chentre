1308bffda57cbc5cd066a91f02de8680694c3445
alexisc <a.chentreu@protonmail.com>
2023-10-22 18:09:34 +0200

    Config nuxtjs/apollo (module graphql) + page Projects added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,25 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     '@nuxt/content',
-    '@tailwindcss/typography'
+    '@tailwindcss/typography',
+    '@nuxtjs/apollo'
   ],
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
       theme: 'nord',
       preload: ['ts','js','css','java','json','bash','vue']
+    }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
     }
   }
 })
---
+++ pages/projects.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="text-3xl my-8">Projets</h1>
+    <p class="text-lg mb-8">Voici mes projets d'école sur Github</p>
+</template>
