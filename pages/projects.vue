1e8f194a74b04f1c813774ae32aba5e4e131b5a2
alexisc <a.chentreu@protonmail.com>
2024-01-23 16:44:52 +0100

    Display Several projects resolved

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,11 @@
 <script setup>
 
 const query = gql`
-query ($id: ID!) {
+query  {
   viewer {
     login
   }
-  node(id: $id) {
+  nodes(ids: ["R_kgDOK5Okug", "R_kgDOKuQjpQ"]) {
     ... on Repository {
       id
       name
@@ -27,8 +27,9 @@ query ($id: ID!) {
 `;
 
 const id = "R_kgDOK972GA";
+const project = ref([]);
 const { data } = await useAsyncQuery(query, { id });
-console.log(data);
+project.value.push(data?.node);
 </script>
 
 <template>
@@ -36,20 +37,20 @@ console.log(data);
       <h1 class="text-3xl my-8">Projets</h1>
       <p class="text-lg mb-8">Voici mes projets d'école sur Github</p>
       <section class="grid grid-cols-1 gap-10">
-        <div class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
-          <a :href="data?.node?.url" target="_blank">
-            <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">{{ data?.node?.name }}</h2>
+        <div v-for="project in data?.nodes" :key="project.id" class="p-8 border-4 my-4 rounded-lg hover:bg-gray-50">
+          <a :href="data?.nodes?.url" target="_blank">
+            <h2 class="text-2xl text-indigo-800 font-semibold mb-2 hover:underline">{{ project.name }}</h2>
           </a>
-          <p>{{ data?.node?.description }}</p>
+          <p>{{ project.description }}</p>
           <div class="mt-4">
             <p>
-              <Icon name="fontisto:star" size="1.1rem" class="text-indigo-700" /> Stars: {{ data?.node?.stargazers?.totalCount }}
+              <Icon name="fontisto:star" size="1.1rem" class="text-indigo-700" /> Stars: {{ project.stargazers?.totalCount }}
             </p>
             <p>
-              <Icon name="system-uicons:branch" size="1.1rem" class="text-indigo-800" /> Forks: {{ data?.node?.forks?.totalCount }}
+              <Icon name="system-uicons:branch" size="1.1rem" class="text-indigo-800" /> Forks: {{ project.forks?.totalCount }}
             </p>
             <p>
-              <Icon name="system-uicons:eye" size="1.1rem" class="text-indigo-700" /> Watchers: {{ data?.node?.watchers?.totalCount }}
+              <Icon name="system-uicons:eye" size="1.1rem" class="text-indigo-700" /> Watchers: {{ project.watchers?.totalCount }}
             </p>
           </div>
         </div>
