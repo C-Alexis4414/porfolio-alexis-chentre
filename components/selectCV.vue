7b91e75c577d7590a3f7f12f61bbb0f8fbc64154
alexisc <a.chentreu@protonmail.com>
2024-02-18 18:21:50 +0100

    Start style CV languages dropdown

--- components/selectCV.vue
+++ components/selectCV.vue
@@ -1,10 +1,22 @@
 <template>
     <div>
-        <div @click="toggleDropdown" class="dropdown-button">Télécharger mon CV</div>
-        <div v-if="isDropdownOpen" class="dropdown-menu">
-            <a href="https://1drv.ms/b/s!AjV8299KVvmVtDNYM9CyZKeDrMBV?e=ZIOa8a" target="_blank" download>Français</a>
+        <div class="text-center bg-slate-600 rounded m-1 w-52" @click="toggleDropdown">Télécharger mon CV</div>
+        <div class="grid grid-cols-1 grid-rows-2 bg-slate-200 rounded-b-lg w-48 mx-3" v-if="isDropdownOpen">
+            <div class="bg-slate-500 m-1 rounded">
+                    <a href="https://1drv.ms/b/s!AjV8299KVvmVtDNYM9CyZKeDrMBV?e=ZIOa8a" target="_blank">
+                            <img class="float-left pr-4 pt-1 pl-1" src="~/assets/images/france.png"/>
+                        Français
+                    </a>
+            </div>
+            <div class="bg-slate-500 m-1 rounded">
+                <p>
+                        <img class="float-left pr-4 pt-1 pl-1" src="~/assets/images/united-kingdom.png"/>
+                    Anglais
+                    <span>Bientôt</span>
+                </p>
+            </div>
         </div>
-        <img src="~/assets/images/cv_fr.jpg" width="1241" height="1755" />    
+        <img class="m-2 rounded-md" src="~/assets/images/cv_fr.jpg" width="1241" height="1755" />    
     </div>
 </template>
 
@@ -13,7 +25,6 @@
         data() {
             return {
                 isDropdownOpen: false,
-                files: ['fr', 'en']
             };
         },
         methods: {
