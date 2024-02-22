980ea16d188fd561614efaa742552a0c84cb1314
alexisc <a.chentreu@protonmail.com>
2024-02-22 19:04:34 +0100

    Fade down effect

--- components/selectCV.vue
+++ components/selectCV.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-center bg-slate-600 rounded m-1 w-52" @click="toggleDropdown">Télécharger mon CV</div>
-        <div class="grid grid-cols-1 grid-rows-2 bg-slate-200 rounded-b-lg w-48 mx-3" v-if="isDropdownOpen">
+        <div class="grid grid-cols-1 grid-rows-2 bg-slate-200 rounded-b-lg w-48 mx-3" :class="{ 'animate-fade-down': isDropdownOpen }" v-if="isDropdownOpen">
             <div class="bg-slate-500 m-1 rounded">
                     <a href="https://1drv.ms/b/s!AjV8299KVvmVtDNYM9CyZKeDrMBV?e=ZIOa8a" target="_blank">
                             <img class="float-left pr-4 pt-1 pl-1" src="~/assets/images/france.png"/>
