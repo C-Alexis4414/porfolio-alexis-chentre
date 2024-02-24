502e17965343f8a707e6a09dacb922af850f8262
alexisc <a.chentreu@protonmail.com>
2024-02-24 22:39:01 +0100

    Stylized green dropdown selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
         boxShadow: {
           'ix2': 'inset 0 3px 6px 0 rgba(0, 0, 0, 0.1)',
         },
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        }
       }
     },
     plugins: [
