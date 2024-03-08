516a1816de0cae19045f886f14f52e35d73853ac
alexisc <a.chentreu@protonmail.com>
2024-03-08 16:17:19 +0100

    Prose-invert with tailwindcss/typography plugin for dark mode rendring

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -10,7 +10,7 @@ const { data } = await useAsyncData(`content-${path}`, () => {
 
 <template>
     <!-- <p>{{ $route.params.slug }}</p> -->
-    <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
+    <ContentRenderer :value="data" class="prose dark:prose-invert my-10 mx-auto max-w-7xl" />
     <div class="my-8">
         <a v-for="tag in  data.tags " :key="tag" :href="`/blog/tags/${tag}`"
         class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
