370b22b11ba9cadfbc4ac4b9d2613c4bcb9c3c4d
alexisc <a.chentre@protonmail.com>
2024-03-15 19:33:01 +0100

    Personalization of my biography with style and contrast

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,9 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
     <section>
         <div class="flex flex-row">
             <HandRaisedIcon class="h-6 w-6 text-blue-500"/>
-            <h1 class="text-emerald-400 font-semibold m-2">Bonjour, je m'appelle Alexis Chentre !</h1>
+            <h1 class="text-3xl text-emerald-500 font-bold m-2">Bonjour, je m'appelle Alexis Chentre !</h1>
         </div>
-        <p class="text-base text-lime-200 m-4 italic">Développeur web</p>
+        <p class="text-2xl text-teal-500 m-4 italic font-semibold dark:text-lime-500">Développeur Aveva en alternance</p>
     </section>
     <section class="flex flex-col items-center">
       <div class="box-border h-52 w-72">
@@ -23,9 +23,13 @@ const { data: posts } = await useAsyncData('latest-posts', () =>
     </section>
     <section class="flex flex-col md:flex-row">
       <div class="md:3/4">
-        <h2 class="text-3xl font bold mt-8">About</h2>
-        <p class="text-justify text-lg py-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec consequat sem. Nam tempor a lacus in tincidunt. Ut eget diam lorem. In faucibus lacinia est, eget condimentum nulla suscipit vel. Curabitur fermentum lorem urna, vitae eleifend ante interdum porttitor. Mauris ut sagittis massa, sed sodales dolor. Quisque porta, quam non sodales mattis, orci odio imperdiet nibh, id feugiat augue enim non dolor. Etiam vitae enim eu libero pharetra elementum quis quis quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin et orci vel tellus facilisis tristique. Aenean mollis sapien eu vulputate commodo. Aenean lacinia vitae purus vel faucibus. Ut ultrices faucibus mauris. Phasellus blandit nunc ante, ut interdum libero lobortis sed. Phasellus nec varius metus, eu ultricies lorem.
+        <h2 class="text-xl font-bold mt-8">À propos de moi</h2>
+        <p class="text-justify text-base py-2">
+          Normand débarqué en Loire-Atlantique depuis 8 ans, je suis issue du secteur des télécommunications en bureau d'études pour le déploiement de la fibre optique.
+          J'ai récemment entrepris un parcours de reconversion par souhait d'exercer un metier plus en adéquation avec mes valeurs, à savoir ma passion pour la tech, choix confirmé lors de ma dernière expérience professionnelle.
+          J'ai eu la chance d'avoir des collègues et des amis développeurs qui m'ont encouragé dans ma démarche. Cela, additionné à mon goût pour la découverte du fonctionnement, la composition de réseaux et d'UC, ainsi que la conception d'applications, j'ai décidé de franchir le pas.
+          C'est ainsi que j'ai intégré la formation de concepteur développeur d'applications à Ada Tech School depuis l'année dernière, en mai 2023, pour une durée de 21 mois.
+          Ce que je cherche dans cette nouvelle carrière, c'est de pouvoir mettre à contribution mon appétence à apprendre continuellement de nouvelles choses et à résoudre des problèmes sur des solutions ingénieuses.
         </p>
       </div>
       <selectCV />
